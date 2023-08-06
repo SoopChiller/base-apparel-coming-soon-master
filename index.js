ae54321ec08f02d941411ab0c0e4e4ca3afa4cab
SoopChiller <superchillermusic@gmail.com>
2023-08-05 18:26:37 -0700

    finishing touches

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function getValue() {
+function desktop() {
   let value = document.getElementById('email-bar').value;
   value = value.toLowerCase();
   let correctFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -7,10 +7,10 @@ function getValue() {
       border: 2px solid  #F96464;
     `
     let arrowButton = document.getElementById('arrow-button').style.cssText = `
-      left: 22.1rem;
+      left: 22rem;
       top: 0.00rem;
       width: 6.25rem;
-      height: 3.70rem;
+      height: 3.3rem;
       flex-shrink: 0
     `
     let validEmail = document.getElementById('valid-email').style.display = 'block';
@@ -31,7 +31,7 @@ function mobile() {
       left: 15.90rem;
       bottom: 2.10rem;
       width: 4rem;
-      height: 3.1rem;
+      height: 2.80rem;
       flex-shrink: 0;
       background-image: linear-gradient(to right,hsl(0, 80%, 86%),  hsl(0, 74%, 74%));
     `
@@ -41,7 +41,7 @@ function mobile() {
 
     let error = document.getElementById('error');
 
-    error.style.cssText = 'display: block; left: 13.70rem';
+    error.style.cssText = 'display: block; left: 13.70rem; top: 0.75rem';
   }
 }
 
